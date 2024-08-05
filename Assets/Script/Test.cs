ba2e8aa8ee37bba6006c05d1786968d041372006
tjdtjq <tjdtjq555555@gmail.con>
2024-08-06 05:18:29 +0900

    no message

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -1,16 +1,79 @@
+using Best.ServerSentEvents;
 using EasyButtons;
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
-    Dictionary<int, int> dats = new Dictionary<int, int>();
+    EventSource _sse;
+
     [Button]
-    public void Credentials(bool flag)
+    public void Json()
     {
-        Debug.Log(1111111);
+        SseMessageResponse response = new SseMessageResponse();
+        response.Id = $"ididid";
+        response.Data = "message 11111";
+        response.Event = "Evet 11111";
+
+        UnityHelper.LogSerialize(response);
+    }
+
+    [Button]
+    public void SseOpen()
+    {
+        _sse = new EventSource(new Uri($"{GameOptionManager.GetCurrentServerUrl}/Sse/Connect"));
+        _sse.Open();
+
+        _sse.OnOpen += OnEventSourceOpened;
+
+        _sse.OnMessage += OnEventSourceMessage;
+
+        _sse.OnError += OnEventSourceError;
+
+        _sse.OnRetry += OnEventSourceRetry;
+    }
+
+    [Button]
+    public void SseClose()
+    {
+        _sse.Close();
+    }
+
+    [Button]
+    public void SendMessage()
+    {
+        SseMessageResponse message = new SseMessageResponse()
+        {
+             Id = "1123123213213",
+             Data = "message 11111",
+             Event = "Evet 11111",
+        };
+
+        Managers.Web.SendPostRequest<string>("Sse/Message", message, (res) => 
+        {
+        });
+    }
+
+    void OnEventSourceOpened(EventSource source)
+    {
+        UnityHelper.Log_H("Connection established!");
+    }
+    void OnEventSourceMessage(EventSource source, Message msg)
+    {
+        UnityHelper.Log_H($"Received LastEventId: {source.LastEventId}");
+        UnityHelper.Log_H($"Received Data: {msg.Data}");
+        UnityHelper.Log_H($"Received Id: {msg.Id}");
+        UnityHelper.Log_H($"Received Event: {msg.Event}");
+    }
+    void OnEventSourceError(EventSource source, string error)
+    {
+        UnityHelper.Log_H($"Error encountered: {error}");
+    }
+    bool OnEventSourceRetry(EventSource source)
+    {
+        UnityHelper.Log_H("Attempting reconnection...");
+        return true;  // Allow reconnection. Returning false will prevent retry.
     }
 }
 [System.Serializable]
---
+++ Assets/Script/Utils/Server/RR/SseMessageResponse.cs
@@ -0,0 +1,10 @@
+﻿public record SseMessageResponse
+{
+    public string Id { get; set; } = null!;
+    public string Event { get; set; } = null!;
+    public string Data { get; set; } = null!;
+}
+public record SseClientId
+{
+    public string ClientId { get; set; } = null!;
+}
