f64724d369775127d131e6c0da4f2499a99d6205
tjdtjq <tjdtjq555555@gmail.con>
2024-11-30 20:14:39 +0900

    no message

--- Assets/Script/Core/Character/Attack/EnemyAttackNomal.cs
+++ Assets/Script/Core/Character/Attack/EnemyAttackNomal.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f0a17c6af2483707ee48da45728fc6d5a28b4cdbaebf41bc970a54cd2b4a18f
-size 1633
+oid sha256:be4f1fe8489c8fe32bc05025e71c0fe42acf14c52eb01205975b4382ec0e1cf9
+size 1637
