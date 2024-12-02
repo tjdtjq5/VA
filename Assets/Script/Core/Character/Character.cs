0392b4e31c4b516c8beb7aded2f9136eeda983cb
tjdtjq <tjdtjq555555@gmail.con>
2024-12-02 17:13:13 +0900

    no message

--- Assets/Script/Core/Character/Character.cs
+++ Assets/Script/Core/Character/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0617fdb27b3f19d23d86cbf121cb37a2e5b59d9f290c716214ae1839c9e15fc
-size 6049
+oid sha256:0ed8bd10ec82d212e981f557e279490bab0b5bd3bcc5f56ff5ec8088f2709b0f
+size 6494
