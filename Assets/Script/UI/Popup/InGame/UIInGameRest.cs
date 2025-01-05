9723ca1c0a194962516ba720cb537ec80ed7ae78
tjdtjq <tjdtjq555555@gmail.con>
2025-01-05 13:57:26 +0900

    no message

--- Assets/Script/UI/Popup/InGame/UIInGameRest.cs
+++ Assets/Script/UI/Popup/InGame/UIInGameRest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f710320a64a79b79e043d9018171d5a3f45ccfb62ead39a1495ea4783cdc1d21
-size 1735
+oid sha256:9c8eae4ddb8c52b5d20b714ed2dec064be63b83ba36c8f931594748dc6cb9fb4
+size 1562
