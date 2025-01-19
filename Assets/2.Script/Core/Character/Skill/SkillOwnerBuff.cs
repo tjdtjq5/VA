617e83340ef5cd0591447ee2884fc11ef3e6f050
tjdtjq <tjdtjq555555@gmail.con>
2025-01-19 10:06:25 +0900

    no message

---
+++ Assets/2.Script/Core/Character/Skill/SkillOwnerBuff.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11af49f1d170fc7820cba1d10b619283524b4479300f9e893ee9cda0a0e4c88c
+size 518
---
+++ Assets/2.Script/Core/Character/Skill/SkillTargetBuff.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a86717d61135c2ee81b68dd325e5e27e4cdd1e26daff1703802a4c19e9686a6c
+size 933
