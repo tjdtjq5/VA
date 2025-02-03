4e1f596a50106711fc24cd76cf778ce9a2b44054
tjdtjq <tjdtjq555555@gmail.con>
2025-02-04 02:15:53 +0900

    no message

--- Assets/2.Script/UI/Main/UILevel.cs
+++ Assets/2.Script/UI/Main/UILevel.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6db1b794d3b4277c1cc170b842d9c7c987c4ac93c22e9b92dda975ffa39b83b
-size 2044
+oid sha256:16498407b8c18921d2079554fd6404b9de745f1153a484edd1086511835a2bd1
+size 2065
