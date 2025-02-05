4d8f598b0ea15751debd04e40a695d3ce5f52a21
tjdtjq <tjdtjq555555@gmail.con>
2025-02-05 18:43:47 +0900

    no message

--- Assets/2.Script/Core/Character/Attack/PlayerAnger.cs
+++ Assets/2.Script/Core/Character/Attack/PlayerAnger.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52d6f1686994cf35cfa551d6efcaf8a926a533e2a84f463bd3d7fa75475049c2
-size 2583
+oid sha256:0a6063111d121d96fbcc35bfd02641393f6cd026706c9196a3b433b80d7b3918
+size 2584
