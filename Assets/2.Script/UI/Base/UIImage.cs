94deeaf609ad1246ae848e2ae532398391981e4a
tjdtjq <tjdtjq555555@gmail.con>
2025-02-07 03:14:19 +0900

    no message

--- Assets/2.Script/UI/Base/UIImage.cs
+++ Assets/2.Script/UI/Base/UIImage.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:374d435e3aa3123bb76d1eab4c33b8100c142e5dacdbedb8a407ed037ccf4229
-size 1847
+oid sha256:816cfa6fbda7f33fa12cd7a261a2be44729e49aa68d5eea10cbe2505180e9480
+size 2036
--- Assets/2.Script/UI/Main/UIPlayer.cs
+++ Assets/2.Script/UI/Main/UIPlayer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:483799050dda297368ce7a116b39b8ea14c547b5f226a1c7b5cbe6d79cbce002
-size 1504
+oid sha256:96825833c3c910a39ef1767d05592b64f89d578d77d5cc52fbe9557a7667714f
+size 2985
