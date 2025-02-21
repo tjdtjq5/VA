02e1abce38a7ac18951958502db3475f1953b2f3
tjdtjq <tjdtjq555555@gmail.con>
2025-02-21 17:10:50 +0900

    no message

--- Assets/2.Script/Core/Character/Attack/Enemy/EnemyAttackBossC1.cs
+++ Assets/2.Script/Core/Character/Attack/Enemy/EnemyAttackBossC1.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33c168022599dabfeb62be42b89c4ce3ebb53fb376f6c72c0a23fd7c7b4a2744
-size 2031
+oid sha256:40bdea495a56c2d584adc558c3f959610295b510a3cd8a035c929683d2f544fe
+size 2533
--- Assets/2.Script/Core/Character/Character.cs
+++ Assets/2.Script/Core/Character/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51f86bc228c76f8deb3f68d1dd55c78ed4eaf1b9d31804038da26acc63654055
-size 21550
+oid sha256:5b1cf34ed293c5f8517337279760d55d01b35e9e8c01b21d33b99eb046d440a2
+size 21569
