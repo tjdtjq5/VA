129d5b93e8d263d565763e8f8d998034a9e111d8
tjdtjq <tjdtjq555555@gmail.con>
2025-03-19 05:10:38 +0900

    no message

--- Assets/2.Script/Core/Character/Buff/BuffStat.cs
+++ Assets/2.Script/Core/Character/Buff/BuffStat.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31cea0c81d95ae1bb77f29365a9904ced1049b446ea47b3c76d28ceb560f814a
-size 1493
+oid sha256:ecee0e456cb6ad56f33acefb4fc5d5c9ec2bb56be7553225ee860ad75c67a0e8
+size 1570
--- Assets/2.Script/Core/Character/Buff/DeBuffStat.cs
+++ Assets/2.Script/Core/Character/Buff/DeBuffStat.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:993396b2e9f23e154de699fe41c34c81b872594510592148b563056dbacc7804
-size 1588
+oid sha256:b255db0863565ba15de39dbcc2b44e9f7f65dedfc4f54afd0991452ca0050068
+size 1664
