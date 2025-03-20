c507f6317c1db467d95a3039b63109b46381105b
tjdtjq <tjdtjq555555@gmail.con>
2025-03-20 15:38:34 +0900

    no message

--- Assets/2.Script/Core/Character/Skill/CharacterSkill.cs
+++ Assets/2.Script/Core/Character/Skill/CharacterSkill.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85defe4f2a38c99406b0054bdbd8bb4a4efd8b46cfd2ba6f181b91c6ecc038e6
-size 6315
+oid sha256:d9e7d7644a610ae9354fc8b8795bc5e54aee9555d77e9a9a645dc26563374b51
+size 6277
