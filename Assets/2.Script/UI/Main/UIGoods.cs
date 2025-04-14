895a72ef7ca7e24943b27500efcfb13de6b2f97f
tjdtjq <tjdtjq555555@gmail.con>
2025-04-15 03:41:27 +0900

    no message

--- Assets/2.Script/UI/Main/UIGoods.cs
+++ Assets/2.Script/UI/Main/UIGoods.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19279f5b4c5ad1a9b7e643666bbfc5e0084f3b93c4c8221a1d21d026dcfec04f
-size 1830
+oid sha256:e2b6eb7be0914d246e602a1c5c6e42b7df8d8e05f4e372318c76abf56485e122
+size 1838
