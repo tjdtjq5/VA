3b7a08cf4353df23cba3ad02def0198eb414e8bf
tjdtjq <tjdtjq555555@gmail.con>
2025-05-07 08:34:55 +0900

    no message

--- Assets/2.Script/Core/Character/Skill/Skill.cs
+++ Assets/2.Script/Core/Character/Skill/Skill.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d80a2f0b9ebef4ed31223a7db126a27f60e344936d6ce51b8fab06abe2f6ab8e
-size 5260
+oid sha256:05cfc3488f9ce8e60d1445091b3b4ddd9571eeb6afccf08fe6183020d656e97b
+size 5267
--- Assets/2.Script/Test/Test.cs
+++ Assets/2.Script/Test/Test.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c4254dfabded63788b149ca3b4f3bfe469b78b0b6ea03abc033a8777f5dcd99
-size 11386
+oid sha256:8061ed656a6ee74068822b0a838ea4b335b377579d29abc4aa0b888f46b8feed
+size 11449
