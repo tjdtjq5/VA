44329482cca4cac7947fb895822dc3e0f7312f50
tjdtjq <tjdtjq555555@gmail.con>
2025-05-18 09:37:40 +0900

    no message

--- Assets/2.Script/Core/Character/Buff/DebuffFire.cs
+++ Assets/2.Script/Core/Character/Buff/DebuffFire.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ded3e17ba2a18576b2ab3367810c796a8ff21654a5df7bc720f255103243f975
-size 1784
+oid sha256:644c957676f9415fa472ce373fb6a77e052b586b6957fe11d3d3e2110afe217f
+size 1774
