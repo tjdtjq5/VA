ed841f3b5378f07bb89acea6cd0ec43bbb60a0b7
tjdtjq <tjdtjq555555@gmail.con>
2025-05-19 22:26:46 +0900

    no message

--- Assets/2.Script/Core/Character/Buff/CharacterBuff.cs
+++ Assets/2.Script/Core/Character/Buff/CharacterBuff.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2287f6ce04db276af4d493daa259f44d6571261d426d258355e1e470efa579d0
-size 7637
+oid sha256:c6f4c733b837d7c1fcae1e4e4a0f070c99982d4db823744cf2e91bb417841d1b
+size 7894
--- Assets/2.Script/Core/Character/Buff/DebuffPoison.cs
+++ Assets/2.Script/Core/Character/Buff/DebuffPoison.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:088ec77c3f6b47d14abf36fb7647af8c49af7e686c7554e269ac05e3b0b43a70
-size 1491
+oid sha256:187939ff4205d017f6334bb33db8db320501c5fb193efbacf9d4f2c9c4b82b90
+size 1487
