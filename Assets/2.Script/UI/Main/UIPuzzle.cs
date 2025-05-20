5ed153bc835d7de7840c6b030e1dec41f9e82b75
DESKTOP-KSC8KNU\s <divingtoshk@gmail.com>
2025-05-20 17:26:05 +0900

    no message

--- Assets/2.Script/UI/Main/UIPuzzle.cs
+++ Assets/2.Script/UI/Main/UIPuzzle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f270b45fab27a8147501d5f65d4110ee3ab5e8ea830721d1be053d05b35023f
-size 24403
+oid sha256:03e380bbfed2f499bff80630b94e24cab79c8a96d84757b98ac60ada676a0267
+size 40205
