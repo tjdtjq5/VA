ec3202facec25b96faa0ddda53b43a139dfe4d94
tjdtjq <tjdtjq555555@gmail.con>
2025-06-06 15:10:20 +0900

    no message

--- Assets/2.Script/Core/Character/Buff/BuffStat.cs
+++ Assets/2.Script/Core/Character/Buff/BuffStat.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bed829cde37775acec4faf9b4d6c27140183d5763986e45e1b1034787d79f3c4
-size 2685
+oid sha256:d0ed016c2bd3e173949367d07d18e2616467aa52312fe7e91783aa1be55e07fc
+size 2755
--- Assets/2.Script/Core/GameUtils/BuffText.cs
+++ Assets/2.Script/Core/GameUtils/BuffText.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2168412fb09a72318b5bf2a554af2808aea6c3a124664d9ed519d4b4ba5c790d
-size 515
+oid sha256:24771205f8855c92bce9f3ae7ac8722263c03552b31baf053f1d3ab0fb531aa9
+size 537
