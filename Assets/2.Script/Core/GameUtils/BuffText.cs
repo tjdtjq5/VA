ecfca465f36b87054e3f30844b76c31f6bd65be8
tjdtjq <tjdtjq555555@gmail.con>
2025-06-07 19:47:29 +0900

    no message

--- Assets/2.Script/Core/GameUtils/BuffText.cs
+++ Assets/2.Script/Core/GameUtils/BuffText.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24771205f8855c92bce9f3ae7ac8722263c03552b31baf053f1d3ab0fb531aa9
-size 537
+oid sha256:6d755aa34187013ce8e1f6efb9f6616621194aaf0ab57a1c245d09856ca06b02
+size 680
