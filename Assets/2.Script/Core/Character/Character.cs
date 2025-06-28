6a4104a186ba7963115c3c47697d1c6af3783788
tjdtjq <tjdtjq555555@gmail.con>
2025-06-28 10:02:13 +0900

    no message

--- Assets/2.Script/Core/Character/Character.cs
+++ Assets/2.Script/Core/Character/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43870d5b65a628225b0fa456fe1304f2cc6c2114af86f7845790e76d458da998
+oid sha256:bf7391cf9cfabcc5e44d8717b38d67fff8221e4aa9c300c1f756ad7d2aeebd74
 size 28492
--- Assets/2.Script/Data/SO/PuzzleBattleStatePlayerAction.cs
+++ Assets/2.Script/Data/SO/PuzzleBattleStatePlayerAction.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20179565f4cefcdf954b90a61d63d36b480eb07a870539885149c5bdb08e23d0
-size 3595
+oid sha256:59b6caff08e0f1d12c6b88c77a9394aa4a526ca7b0f6c9f66a2c49acc1783341
+size 3599
