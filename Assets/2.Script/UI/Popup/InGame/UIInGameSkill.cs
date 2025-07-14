ece722e6d6e717b9e31a8959ae63322efd52a27e
tjdtjq <tjdtjq555555@gmail.con>
2025-07-14 13:07:37 +0900

    no message

--- Assets/2.Script/UI/Popup/InGame/UIInGameSkill.cs
+++ Assets/2.Script/UI/Popup/InGame/UIInGameSkill.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1f2d56f06a144b4bf260b219115f988b23fbd92f3a5b32b461a15e55b35edc7
-size 7232
+oid sha256:981901443659a46f7e020c250ce228179625572defeeccff13b61d1d87c65c8f
+size 6765
