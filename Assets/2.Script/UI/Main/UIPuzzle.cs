140c04fcacf017f14c0e48f97a3149aecf1a3cae
tjdtjq <tjdtjq555555@gmail.con>
2025-07-22 18:11:20 +0900

    no message

--- Assets/2.Script/UI/Main/UIPuzzle.cs
+++ Assets/2.Script/UI/Main/UIPuzzle.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0e726dba58db85030372e6f00eed94c164550673a9517a787d55d88e10977d3
-size 31313
+oid sha256:fb19508cf1b338f532a02c21d65d95019a46aec56c6b9b630fbae28695cef201
+size 33745
--- Assets/2.Script/UI/Main/UIPuzzleItem.cs
+++ Assets/2.Script/UI/Main/UIPuzzleItem.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aff0697c75853f6396edadce411c2d41a08e094f34cc243dde279ba0e7e7c133
-size 4934
+oid sha256:ee663b32af75cd89c4c6888f69c19053b4f4013e4fa839d69a791e1bb799a512
+size 6001
