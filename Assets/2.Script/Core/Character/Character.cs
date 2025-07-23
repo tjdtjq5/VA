af0d45f730f39c4ed5701119bc63ee943654f423
tjdtjq <tjdtjq555555@gmail.con>
2025-07-23 12:00:06 +0900

    no message

--- Assets/2.Script/Core/Character/Character.cs
+++ Assets/2.Script/Core/Character/Character.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b14c09482bf2f3daf909feeed2694ebdc57b5a2dc95ed4ff315f62e67e3cf82f
-size 24400
+oid sha256:1ad9466d4e446d1b829e4781314b79305abdd50458acb186654d8308089abf37
+size 25175
--- Assets/2.Script/Manager/FloatingTextManager.cs
+++ Assets/2.Script/Manager/FloatingTextManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dcfbd73b62b5f574d9107189b88aa9594c965e737b1362a685804b8dbb8b68a5
-size 1717
+oid sha256:504fe939e010bfc9c962bdaa6308abb08b652cc091761792a445d81e26365f2e
+size 1729
