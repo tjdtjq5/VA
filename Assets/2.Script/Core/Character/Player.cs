8a3fc9819a244e5f502aaaaff88276fff695e660
tjdtjq <tjdtjq555555@gmail.con>
2025-08-10 19:52:53 +0900

    no message

--- Assets/2.Script/Core/Character/Player.cs
+++ Assets/2.Script/Core/Character/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:076a23751f671ba9cafdd6fa5cb8e3afc41c39b14f0a8ab77d312948d85373bd
-size 18295
+oid sha256:20dda17bd17360dd85c7e2cc98fbd588167e9bf5070201f40d33805b4dcfd460
+size 18724
--- Assets/2.Script/UI/Popup/Robby/Grow/UIGrowView.cs
+++ Assets/2.Script/UI/Popup/Robby/Grow/UIGrowView.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:561f0a0e06a61c355ba15e899fc1f69d0c065901a1d6488eeeb3e3216a7ee55f
-size 2412
+oid sha256:a4a72714eef8097c1ccaf1bfccd6eca37bedb5aad142a63888e988c7f6e8c809
+size 2507
