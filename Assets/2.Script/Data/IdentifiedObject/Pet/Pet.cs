3ea2a3821fd333fe2c2092547e3b699b8b924342
tjdtjq <tjdtjq555555@gmail.con>
2025-08-27 20:07:22 +0900

    no message

---
+++ Assets/2.Script/Data/IdentifiedObject/Pet/Pet.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:38b55110638ef32d8fbf8d16a00296b4e7166344cd0d7bb51a6a8190dd44c207
+size 129
---
+++ Assets/2.Script/UI/Card/Pet/PetCard.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2e59a1d84ec70dee3f5686a9243f8972f63bb1b8015b07152b9598136e7214a6
+size 238
