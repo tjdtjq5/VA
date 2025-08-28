eb558bb867ee72755d4b8b6b0f5edccddafec3d1
tjdtjq <tjdtjq555555@gmail.con>
2025-08-29 08:56:02 +0900

    no message

--- Assets/2.Script/UI/Card/Pet/PetEquipCard.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:da32aba6e32c7ae9240079662c67e9e098ea2a2e0e7f0a3c362be0c91f2aa644
-size 373
--- Assets/2.Script/UI/Card/Pet/PetInfoCard.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d6e4217285cba2b3662e2508375961dcd8853f6462da3051e00e276073e579a2
-size 128
